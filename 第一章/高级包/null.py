593b371ccf8dd4230d1b5aa078d3474eb6d9e95a
wangxinyue <wangxinyue@winwin-inc.com>
2023-05-19 18:54:44 +0800

    RPA

---
+++ 第一章/高级包/null.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+url = 'http://ml-jsonrpc.banmahui.cn/resolve-chaos-word'
+headers = {'content-type': 'application/json', 'apikey': 'Rahm3xu6Ohng5ohm9Ieth5leeghahD8i'}
+payload = {
+    'method': 'resolve_chaos_word',
+    'params': ['成科研果'],
+    'id': 1,
+    'jsonrpc': '2.0'
+}
+
+response = requests.post(url, headers=headers, data=json.dumps(payload))
+result = response.json()
+
+print(result['result'])
---
+++ 第一章/高级包/test.py
@@ -0,0 +1,5 @@
+my_list = {"北": [100, 50], "京": [30, 200], "市": [25, 600]}
+for my_str in my_list:
+    ZuobiaoX = my_list[my_str][0]
+    ZuobiaoY = my_list[my_str][1]
+    print(f"字符名称是 {my_str} \nX轴坐标是 {ZuobiaoX} \nY轴坐标是 {ZuobiaoY}")
